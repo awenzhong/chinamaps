c0abe48830775f9f81760d7ec5a34088e6ebc938
i.xucheng <i.xucheng@13addb27-d54e-0410-a380-5f62f0559d5a>
2008-06-05 15:19:19 +0000

    Add file for global data file

---
+++ mapplets/euro2008/euro08_data.js
@@ -0,0 +1,205 @@
+{
+  "blogs": {
+    "ALL": {
+      "blog_id": "5555785845362919586",
+      "blog_url": "http://euromappletall.blogspot.com/",
+      "data_path":"2008/05/dataall.html"
+    }
+  },
+  "marker": {
+    "pos":
+    {
+      "lat": 47.06069439255876,
+      "lng": 9.607093334197998
+    }
+  },
+  "teams": {
+    "aut" : {
+      "pos":
+      {
+        "lat": 48.188063481211415,
+        "lng": 16.3916015625
+      }
+    },
+    "pol" : {
+      "pos":
+      {
+        "lat": 52.245460541432486,
+        "lng": 21.012039184570312
+      }
+    },
+    "por" : {
+      "pos":
+      {
+        "lat": 38.70667813760075,
+        "lng": -9.1351318359375
+      }
+    },
+    "ita" : {
+      "pos":
+      {
+        "lat": 41.896144026994854,
+        "lng": 12.48321533203125
+      }
+    },
+    "fra" : {
+      "pos":
+      {
+        "lat": 48.85703523304219,
+        "lng": 2.3503875732421875
+      }
+    },
+    "gre" : {
+      "pos":
+      {
+        "lat": 37.97992751270058,
+        "lng": 23.717079162597656
+      }
+    },
+    "tur" : {
+      "pos":
+      {
+        "lat": 39.918162846609455,
+        "lng": 32.84912109375
+      }
+    },
+    "cze" : {
+      "pos":
+      {
+        "lat": 50.088868933829644,
+        "lng": 14.420928955078125
+      }
+    },
+    "ger" : {
+      "pos":
+      {
+        "lat": 52.52457705042609,
+        "lng": 13.412933349609375
+      }
+    },
+    "cro" : {
+      "pos":
+      {
+        "lat": 45.7943396304607,
+        "lng": 15.9576416015625
+      }
+    },
+    "rus" : {
+      "pos":
+      {
+        "lat": 55.753395077731085,
+        "lng": 37.618560791015625
+      }
+    },
+    "spa" : {
+      "pos":
+      {
+        "lat": 40.413496049701955,
+        "lng": -3.698272705078125
+      }
+    },
+    "swe" : {
+      "pos":
+      {
+        "lat": 59.331788517964924,
+        "lng": 18.065643310546875
+      }
+    },
+    "rom" : {
+      "pos":
+      {
+        "lat": 44.44897735945844,
+        "lng": 26.096649169921875
+      }
+    },
+    "ned" : {
+      "pos":
+      {
+        "lat": 52.377275885161175,
+        "lng": 4.89990234375
+      }
+    },
+    "sui": {
+      "pos":
+      {
+        "lat": 46.948856043101436,
+        "lng": 7.4494171142578125
+      }
+    }
+  },
+  "stadiums": {
+    "sal": {
+      "pos": {
+        "lat": 47.81622373770088,
+        "lng": 12.998199462890625
+      },
+      "cap": 30000,
+      "seats_cov": 30000,
+      "seats": 30000
+    },
+    "zrh": {
+      "pos": {
+        "lat": 47.38280191369409,
+        "lng": 8.504308462142944
+      },
+      "cap": 30000,
+      "seats_cov": 30000,
+      "seats": 30000
+    },
+    "gen": {
+      "pos": {
+        "lat": 46.177839502424064,
+        "lng": 6.127474308013916
+      },
+      "cap": 30000,
+      "seats_cov": 30000,
+      "seats": 30000
+    },
+    "bas": {
+      "pos": {
+        "lat": 47.54159204396787,
+        "lng": 7.620112895965576
+      },
+      "cap": 42500,
+      "seats_cov": 42500,
+      "seats": 42500
+    },
+    "ber": {
+      "pos": {
+        "lat": 46.963158001062254,
+        "lng": 7.464812994003296
+      },
+      "cap":32000 ,
+      "seats_cov": 32000,
+      "seats": 32000
+    },
+    "inn": {
+      "pos": {
+        "lat": 47.25597565727707,
+        "lng": 11.411254405975342
+      },
+      "cap": 30000,
+      "seats_cov": 30000,
+      "seats": 30000
+    },
+    "kla": {
+      "pos": {
+        "lat": 46.60891393194792,
+        "lng": 14.278256893157959
+      },
+      "cap": 32000,
+      "seats_cov": 30000,
+      "seats": 30000
+    },
+    "vie": {
+      "pos": {
+        "lat": 48.2071864558831,
+        "lng": 16.42104685306549
+      },
+      "cap": 53000,
+      "seats_cov": 50000,
+      "seats": 50000
+    }
+  },
+  players: {"ger":{"k":["Adler, René","Enke, Robert","Lehmann, Jens"],"t":"Löw, Joachim","m":["Ballack, Michael","Borowski, Tim","Frings, Torsten","Hitzlsperger, Thomas","Odonkor, David","Rolfes, Simon","Schweinsteiger, Bastian","Trochowski, Piotr"],"d":["Friedrich, Arne","Fritz, Clemens","Jansen, Marcell","Lahm, Philipp","Mertesacker, Per","Metzelder, Christoph","Westermann, Heiko"],"f":["Gomez, Mario","Klose, Miroslav","Kuranyi, Kevin","Neuville, Oliver","Podolski, Lukas"]},"fra":{"k":["Coupet, Gregory","Frey, Sebastien","Mandanda, Steve"],"t":"Domenech, Raymond","m":["Diarra, Lassana","Govou, Sidney","Makelele, Claude","Malouda, Florent","Nasri, Samir","Ribery, Franck","Toulalan, Jeremy","Vieira, Patrick"],"d":["Abidal, Eric","Boumsong, Jean Alain","Clerc, Francois","Evra, Patrice","Gallas, William","Sagnol, Willy","Squillaci, Sebastien","Thuram, Lilian"],"f":["Anelka, Nicolas","Benzema, Karim","Gomis, Bafetimbi","Henry, Thierry"]},"rom":{"k":["Lobont, Bogdan","Popa, Marius","Stancioiu, Eduard"],"t":"Piturca, Victor","m":["Chivu, Cristian","Cocis, Razvan","Codrea, Paul","Cristea, Adrian","Dica, Nicolae","Nicolita, Banel","Petre, Florentin","Radoi, Mirel"],"d":["Contra, Cosmin","Ghionea, Sorin","Goian, Dorin","Moti, Cosmin","Radu, Stefan","Rat, Razvan","Sapunaru, Cristian","Tamas, Gabriel"],"f":["Marica, Ciprian","Mutu, Adrian","Niculae, Daniel","Niculae, Marius"]},"swe":{"k":["Isaksson, Andreas","Shaaban, Rami","Wiland, Johan"],"t":"Lagerbäck, Lars","m":["Alexandersson, Niclas","Andersson, Daniel","Källström, Kim","Larsson, Sebastian","Linderoth, Tobias","Ljungberg, Fredrik","Svensson, Anders","Wilhelmsson, Christian"],"d":["Dorsin, Mikael","Granqvist, Andreas","Hansson, Petter","Majstorovic, Daniel","Mellberg, Olof","Nilsson, Mikael","Stoor, Fredrik"],"f":["Allbäck, Marcus","Elmander, Johan","Ibrahimovic, Zlatan","Larsson, Henrik","Rosenberg, Markus"]},"ned":{"k":["Stekelenburg, Maarten","Timmer, Henk","van der Sar, Edwin"],"t":"Van Basten, Marco","m":["Afellay, Ibrahim","de Jong, Nigel","de Zeeuw, Demy","Engelaar, Orlando","Robben, Arjen","Sneijder, Wesley","van Bronckhorst, Giovanni","van der Vaart, Rafael","van Persie, Robin"],"d":["Boulahrouz, Khalid","Bouma, Wilfred","de Cler, Tim","Heitinga, John","Mathijsen, Joris","Melchiot, Mario","Ooijer, André"],"f":["Huntelaar, Klaas Jan","Kuijt, Dirk","van Nistelrooy, Ruud","Vennegoor of Hesselink, Jan"]},"por":{"k":["Quim","Ricardo","Rui Patricio"],"t":"Scolari, Luiz Felipe","m":["Deco","Joao Moutinho","Miguel Veloso","Petit","Raul Meireles"],"d":["Bosingwa, José","Bruno Alves","Fernando Meira","Jorge Ribeiro","Miguel","Paulo Ferreira","Pepe","Ricardo Carvalho"],"f":["Cristiano Ronaldo","Helder Postiga","Hugo Almeida","Nani","Nuno Gomes","Ricardo Quaresma","Simao"]},"sui":{"k":["Benaglio, Diego","Jakupovic, Eldin","Zuberbühler, Pascal"],"t":"Kuhn, Jakob","m":["Barnetta, Tranquillo","Behrami, Valon","Cabanas, Ricardo","Fernandes, Gelson","Gygax, Daniel","Huggel, Benjamin","Inler, Gökhan","Vonlanthen, Johan","Yakin, Hakan"],"d":["Degen, Philipp","Djourou, Johan","Grichting, Stephane","Lichtsteiner, Stephan","Magnin, Ludovic","Müller, Patrick","Senderos, Philippe","Spycher, Christoph"],"f":["Derdiyok, Eren","Frei, Alexander","Streller, Marco"]},"ita":{"k":["Amelia, Marco","Buffon, Gianluigi","de Sanctis, Morgan"],"t":"Donadoni, Roberto","m":["Ambrosini, Massimo","Aquilani, Alberto","Camoranesi, Mauro German","de Rossi, Daniele","di Natale, Antonio","Gattuso, Gennaro","Perrotta, Simone","Pirlo, Andrea"],"d":["Barzagli, Andrea","Cannavaro, Fabio","Chiellini, Giorgio","Grosso, Fabio","Materazzi, Marco","Panucci, Christian","Zambrotta, Gianluca"],"f":["Borriello, Marco","Cassano, Antonio","del Piero, Alessandro","Quagliarella, Fabio","Toni, Luca"]},"pol":{"k":["Boruc, Artur","Fabianski, Lukasz","Kuszczak, Tomasz"],"t":"Beenhakker, Leo","m":["Blaszczykowski, Jakub","Dudka, Dariusz","Gargula, Lukasz","Krzynowek, Jacek","Lewandowski, Mariusz","Murawski, Rafal","Pazdan, Michal","Roger Guerreiro"],"d":["Bak, Jacek","Golanski, Pawel","Jop, Mariusz","Kokoszka, Adam","Wasilewski, Marcin","Wawrzyniak, Jakub","Zewlakow, Michal"],"f":["Lobodzinski, Wojciech","Saganowski, Marek","Smolarek, Euzebiusz","Zahorski, Tomasz","Zurawski, Maciej"]},"gre":{"k":["Chalkias, Kostas","Nikopolidis, Antonis","Tzorvas, Alexandros"],"t":"Rehhagel, Otto","m":["Basinas, Angelos","Giannakopoulos, Stelios","Karagounis, Georgios","Katsouranis, Konstantinos","Tziolis, Alexandros"],"d":["Antzas, Paraskevas","Dellas, Traianos","Goumas, Ioannis","Kyrgiakos, Sotirios","Patsatzoglu, Christos","Seitaridis, Georgios","Spiropoulos, Nikos","Torosidis, Vassilis","Vyntra, Loukas"],"f":["Amanatidis, Ioannis","Charisteas, Angelos","Gekas, Theofanis","Liberopoulos, Nikos","Salpingidis, Dimitrios","Samaras, Georgios"]},"aut":{"k":["Macho, Jürgen","Manninger, Alexander","Özcan, Ramazan"],"t":"Hickersberger, Josef","m":["Aufhauser, René","Fuchs, Christian","Ivanschitz, Andreas","Korkmaz, Ümit","Leitgeb, Christoph","Säumel, Jürgen","Standfest, Joachim","Vastic, Ivica"],"d":["Garics, György","Gercaliu, Ronald","Hiden, Martin","Katzer, Markus","Patocka, Jürgen","Pogatetz, Emanuel","Prödl, Sebastian","Stranzl, Martin"],"f":["Harnik, Martin","Hoffer, Erwin","Kienast, Roman","Linz, Roland"]},"spa":{"k":["Casillas, Iker","Palop, Andres","Reina"],"t":"Aragones, Luis","m":["de la Red","Fabregas, Cesc","Iniesta","Marcos Senna","Santi Cazorla","Silva, David","Xabi Alonso","Xavi"],"d":["Albiol, Raul","Arbeloa","Capdevila","Fernando Navarro","Juanito","Marchena, Carlos","Puyol","Sergio Ramos"],"f":["Fernando Torres","Guiza","Sergio Garcia","Villa, David"]},"tur":{"k":["Rüstü","Tolga Zengin","Volkan Demirel"],"t":"Terim, Fatih","m":["Altintop, Hamit","Arda Turan","Ayhan Akman","Emre","Kazim Kazim","Mehmet Aurelio","Mehmet Topal","Tümer Metin","Tuncay Sanli"],"d":["Emre Asik","Gökhan Zan","Güngör, Emre","Hakan Balta","Sabri Sarioglu","Servet Cetin","Ugur Boral"],"f":["Erdinc, Mevlüt","Gökdeniz Karadeniz","Nihat","Semih Sentürk"]},"rus":{"k":["Akinfeev, Igor","Gabulov, Vladimir","Malafeev, Vyacheslav"],"t":"Hiddink, Guus","m":["Bilyaletdinov, Diniyar","Bystrov, Vladimir","Semak, Sergey","Semshov, Igor","Shirokov, Roman","Torbinskiy, Dmitriy","Zhirkov, Yuriy","Zyryanov, Konstantin"],"d":["Anyukov, Aleksandr","Berezutskiy, Aleksey","Berezutskiy, Vasiliy","Ignashevich, Sergey","Kolodin, Denis","Yanbaev, Renat"],"f":["Adamov, Roman","Arshavin, Andrey","Pavlyuchenko, Roman","Pogrebnyak, Pavel","Saenko, Ivan","Sychev, Dmitriy"]},"cro":{"k":["Galinovic, Mario","Pletikosa, Stipe","Runje, Vedran"],"t":"Bilic, Slaven","m":["Kovac, Niko","Kranjcar, Niko","Leko, Jerko","Modric, Luka","Pokrivac, Nikola","Pranjic, Danijel","Rakitic, Ivan","Srna, Dario","Vukojevic, Ognjen"],"d":["Corluka, Vedran","Knezevic, Dario","Kovac, Robert","Simic, Dario","Simunic, Josip","Vejic, Hrvoje"],"f":["Budan, Igor","Kalinic, Nikola","Klasnic, Ivan","Olic, Ivica","Petric, Mladen"]},"cze":{"k":["Blazek, Jaromir","Cech, Petr","Zitka, Daniel"],"t":"Brückner, Karel","m":["Galasek, Tomas","Jarolim, David","Matejovsky, Marek","Plasil, Jaroslav","Polak, Jan","Sionko, Libor","Skacel, Rudolf"],"d":["Grygera, Zdenek","Jankulovski, Marek","Kadlec, Michal","Kovac, Radoslav","Pospech, Zdenek","Rozehnal, David","Sivok, Tomas","Ujfalusi, Tomas"],"f":["Baros, Milan","Fenin, Martin","Koller, Jan","Sverkos, Vaclav","Vlcek, Stanislav"]}}
+}
